b33b965fa65b557c706179ddebf520c9ac1d5b5d
Gary O'Neall <gary@sourceauditor.com>
2022-10-27 17:24:15 -0700

    Add more checks for URI types in upgrader
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/main/java/org/spdx/spdxRdfStore/CompatibilityUpgrader.java
+++ src/main/java/org/spdx/spdxRdfStore/CompatibilityUpgrader.java
@@ -132,7 +132,8 @@ public class CompatibilityUpgrader {
 	            	Resource existingRelationship = stmt.getObject().asResource();
 	            	Resource relatedElement = existingRelationship.getPropertyResourceValue(model.createProperty(SpdxConstants.SPDX_NAMESPACE + SpdxConstants.PROP_RELATED_SPDX_ELEMENT));
 	            	Resource relationshipType = existingRelationship.getPropertyResourceValue(model.createProperty(SpdxConstants.SPDX_NAMESPACE + SpdxConstants.PROP_RELATIONSHIP_TYPE));
-	            	if (relatedElement.isURIResource() && relatedElement.getURI().equals(file.getURI()) &&
+	            	if (relatedElement.isURIResource() && file.isURIResource() && relationshipType.isURIResource() &&
+	            			relatedElement.getURI().equals(file.getURI()) &&
 	            			relationshipType.getURI().equals(RelationshipType.CONTAINS.getIndividualURI())) {
 	            		foundContainsRelationships.add(stmt);
             		}
@@ -292,7 +293,7 @@ public class CompatibilityUpgrader {
 	                statementsToRemove.add(iter.next());
 	            }
 	            Resource pkg = convertDoapProjectToSpdxPackage(model, doapProject, docNamespace + idPrefix + Integer.toString(nextSpdxIdNum));
-	            if (!addedDoapProjects.contains(pkg.getURI())) {
+	            if (pkg.isURIResource() && !addedDoapProjects.contains(pkg.getURI())) {
 	                addedDoapProjects.add(pkg.getURI());
 	                nextSpdxIdNum = getNexId(model, docNamespace, idPrefix, nextSpdxIdNum);
 	                Resource relationship = createRelationship(model, pkg, RelationshipType.GENERATED_FROM);
